72a6fb436b26e5a95a72e25cf870ce54d7aa4077
tosiki <tosiki5880@gamil.com>
2024-05-24 10:59:39 +0900

    ui変更

--- sample_app/views.py
+++ sample_app/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render  
 from django.views import View  
+
   
 class SampleView(View):  
 	def get(self, request, *args, **kwargs):  
 		return render(request, 'sample_app/top_page.html')
 top_page = SampleView.as_view()
+
+
 
 
 def index(request):
